503668d35d38be9eaccc79df298d0289841bd212
lidingding2 <damonding1997@gmail.com>
2023-02-21 01:18:00 -0800

    userIcon_update

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,29 @@
 import React from "react";
 import MyCart from "./MyCart";
-import { Button, Tooltip, Space } from "antd";
+import { Button, Tooltip, Space, Dropdown} from "antd";
 import logo from "../assets/images/logo.svg";
 import CreatePostButton from "./CreatePostButton";
 import { LogoutOutlined, UserOutlined, HomeOutlined } from "@ant-design/icons";
 
 function TopBar(props) {
   const { isLoggedIn, handleLogout, handleLoggedIn } = props;
+  
+  // the following items are the options showing up when move to the icon of the user
+  const items = [
+    {
+      key: '1',
+      label: (
+        <a href="/home">Profile</a>
+      ),
+    },
+
+    {
+      key: '2',
+      label: "Logout",
+      onClick: handleLogout,
+      
+    },
+  ];
   return (
     <header className="App-header">
       <Tooltip title="Home">
@@ -21,17 +38,28 @@ function TopBar(props) {
       {/** */}
 
       {/*to login page*/}
-      <Tooltip title="Account">
-        <Button
-          shape="circle"
-          icon={<UserOutlined />}
-          onClick={handleLogout}
-        ></Button>
-      </Tooltip>
       <CreatePostButton/>
-      <MyCart />
+      <MyCart className="myCart"/>
+
+      {/* this following is the code for the dropdown button for user icon */}
+      <Space direction="vertical">
+        <Space wrap>
+          <Dropdown
+            menu={{
+              items,
+            }}
+          placement="bottomLeft">
+          <Button 
+            shape="circle"
+            icon={<UserOutlined />}>
+          </Button>
+          </Dropdown>
+        </Space>
+      </Space>
     </header>
   );
 }
 
 export default TopBar;
+
+
