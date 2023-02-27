7d13c2501f66133d6d5d1ab622c2c8b1339476fa
fhlt <hongf2000@gmail.com>
2023-02-27 12:59:38 -0800

    change on register

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Form, Input, Button, message } from "antd";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
+import { signup } from "../utils";
 import { BASE_URL } from "../constants";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
 
 const formItemLayout = {
   //label 所占百分比
@@ -26,40 +27,21 @@ const tailFormItemLayout = {
     },
     sm: {
       span: 16,
-      offset: 8,
+      offset: 0,
     },
   },
 };
 
 function Register(props) {
   const [form] = Form.useForm();
-
-  const onFinish = (values) => {
-    console.log("Received values of form: ", values);
-    const { username, password } = values;
-    const opt = {
-      method: "POST",
-      url: `${BASE_URL}/signup`,
-      data: {
-        username: username,
-        password: password,
-      },
-      headers: { "content-type": "application/json" },
-    };
-
-    axios(opt)
-      .then((response) => {
-        console.log(response);
-        // case1: registered success
-        if (response.status === 200) {
-          message.success("Registration succeed!");
-          props.history.push("/login");
-        }
+  const onFinish = (data) => {
+    signup(data)
+      .then(() => {
+        message.success("Registration succeed!");
+        props.history.push('/login');
       })
-      .catch((error) => {
-        console.log("register failed: ", error.message);
-        message.success("Registration failed!");
-        // throw new Error('Signup Failed!')
+      .catch((err) => {
+        message.error(err.message);
       });
   };
 
@@ -72,55 +54,52 @@ function Register(props) {
       className="register"
     >
       <Form.Item
-        name="username"
-        label="Username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Username!",
-          },
-        ]}
+        name="email"
+        rules={[{ required: true, message: "Please input your email!" }]}
       >
-        <Input />
+        <Input prefix={<UserOutlined />} placeholder="Email" />
+      </Form.Item>
+
+      <Form.Item
+        name="firstName"
+        rules={[{ required: true, message: "Please input your first name!" }]}
+      >
+        <Input placeholder="firstname" />
+      </Form.Item>
+
+      <Form.Item
+        name="lastName"
+        rules={[{ required: true, message: "Please input your last name!" }]}
+      >
+        <Input placeholder="lastname" />
       </Form.Item>
 
       <Form.Item
         name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-        hasFeedback
+        rules={[{ required: true, message: "Please input your password!" }]}
       >
-        <Input.Password />
+        <Input prefix={<LockOutlined />} placeholder="Password" />
       </Form.Item>
 
       <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={["password"]}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please confirm your password!",
-          },
-          ({ getFieldValue }) => ({
-            validator(rule, value) {
-              if (!value || getFieldValue("password") === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(
-                "The two passwords that you entered do not match!"
-              );
-            },
-          }),
-        ]}
+        name="phone"
+        rules={[{ required: true, message: "Please input your phone!" }]}
       >
-        <Input.Password />
+        <Input placeholder="phone" />
+      </Form.Item>
+
+      <Form.Item
+        name="userName"
+        rules={[{ required: true, message: "Please input your userName!" }]}
+      >
+        <Input placeholder="userName" />
+      </Form.Item>
+
+      <Form.Item
+        name="address"
+        rules={[{ required: true, message: "Please input your address!" }]}
+      >
+        <Input placeholder="address" />
       </Form.Item>
 
       <Form.Item {...tailFormItemLayout}>
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const TOKEN_KEY = "token";
-export const BASE_URL = "http://35.231.175.116:8080";
+export const BASE_URL = "http://localhost:8081";
 export const SEARCH_KEY = {
   all: 0,
   keyword: 1,
